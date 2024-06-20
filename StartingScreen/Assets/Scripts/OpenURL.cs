a13b8191a2127bd89fbf629709d18dc96892775b
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2024-06-20 17:39:32 +0800

    Test Unity to Website connection

--- StartingScreen/Assets/Scripts/OpenURL.cs
+++ StartingScreen/Assets/Scripts/OpenURL.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class OpenURL : MonoBehaviour
 {
   //TO DO: Change the url to the appropriate websites
-  public void OpenWebsite(string url = "https://www.youtube.com/watch?v=K52l9P19_2o&t=189s")
+  public void OpenWebsite(string url)
   {
     Application.OpenURL(url);
     Debug.Log("Pressed button");
